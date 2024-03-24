8b1c02cc46f163c3295f1c8a1eb2cbf8e97f5646
Krishna Adnyana <krishnaadnyanaptr@gmail.com>
2024-03-24 11:13:37 +0800

    log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,8 +7,12 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-pwa';
+
+  ngOnInit(): void {
+    console.log(isDevMode(), 'DEV MODE ACTIVATED')
+  }
 }
